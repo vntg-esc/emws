913bb639923f31ab47c863d96496df2c69d2b6ce
hyunhee-lee <hyunhee.lee@vntgcorp.com>
2021-09-04 08:49:56 +0900

    CHG: create_twitt_info 키 값 잘못된 수정 정정

--- main.py
+++ main.py
@@ -50,101 +50,6 @@ class twittPostInfo():
     def get_retweet_count(self):
         return self._details.get('retweet_count')
 
-def save_data_on_spreadsheet(twitt_days_info):
-    """구글스프레드시트 저장"""
-    scope = [
-    'https://spreadsheets.google.com/feeds',
-    'https://www.googleapis.com/auth/drive',
-    ]
-
-    json_file_name = 'C:\GitHub\VNTG-N-ERP\emws\gspread.json'
-
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(json_file_name, scope)
-    gc = gspread.authorize(credentials)
-
-
-    spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1Z5yePPQLSJOpPxAOHWv4mTQJXxw_vUEjIFKBcqIzqA0/edit#gid=0'
-
-    # 스프레스시트 문서 가져오기 
-    doc = gc.open_by_url(spreadsheet_url)
-
-    # 스프레드시트 문서명
-    worksheetName = datetime.strftime(now_time, '%Y%m')
-
-    # 시트 선택하기
-    worksheet = doc.worksheet(worksheetName)
-
-    # 시트 자료 가져오기
-    worksheet_datas = worksheet.get_all_records()
-
-    # # 1일이면 시트 생성
-    # if datetime.strftime(now_time, '%d') == '1':
-    #     # 시트 생성
-    #     worksheet = doc.add_worksheet(title=worksheetName, rows='1000', cols='22')
-    #     # 타이틀 추가
-    #     worksheet.append_row(constant.C_SPREADSHEET_TITLE)
-    
-    # 스프레드시트 작성
-    for post_date, hour_data in sorted(twitt_days_info.items()):
-        # print(post_date, hour_data)
-        for hour, campaign_data in sorted(hour_data.items()):
-            # print(post_date, hour, campaign_data)
-            for campaign, post_data in campaign_data.items():
-
-                # 추가 작성 필요 여부
-                appendYN = True
-
-                # 스프레드시트 내용 확인
-                for worksheet_data in worksheet_datas:
-                    # 스프레드시트의 일자/시간과 수집한 일자/시간과 일치하는지 확인
-                    if worksheet_data['게시일자'] == str(post_date) and worksheet_data['시간(24시)'] == int(hour):
-                        ##### 업데이트 필요
-                        # print('존재')
-                        appendYN = False
-
-                        # 스프레드시트 행 수 ( +2 : 인덱스 0부터 시작 / 타이틀 )
-                        rowCnt = worksheet_datas.index(worksheet_data) + 2
-
-                        # 스프레드시트 열
-                        colName = "H"
-
-                        # 게시일자 + n일이 수집일자와 같은지 확인
-                        for i in range(0, 7):
-                            if (datetime.strptime(worksheet_data['게시일자'], '%Y-%m-%d') + timedelta(days=i+1) == 
-                                datetime.strptime(datetime.strftime(now_time, '%Y-%m-%d'), '%Y-%m-%d')):
-                                # print('게시일자 + {}일이 수집일자와 동일'.format(i+1))
-                                colName = chr(ord(colName) + i)
-
-                                # 스프레드시트 작성
-                                worksheet.update_acell(f'{colName}{rowCnt}', list(post_data.values())[1] + list(post_data.values())[2])
-
-                                pass
-
-                if appendYN == True:
-                    ##### 신규 건 스프레드시트 작성
-                    print('신규')
-                    # 작성
-                    worksheet.append_row([
-                                        #   datetime.strftime(now_time, '%Y-%m-%d %H:%M:%S')
-                                        datetime.strftime(now_time, '%Y-%m-%d %H')
-                                        , str(post_date)
-                                        , str(hour)
-                                        , str(campaign)
-                                        , list(post_data.values())[0]
-                                        , list(post_data.values())[1]
-                                        , list(post_data.values())[2]
-                                        , "", "", "", "", "", "", ""
-                                        , "추가"
-                                        ])
-
-    print('스프레드시트 작성 종료')
-
-    # 메일 발송
-    send_email_result = send_email.send_email(constant.C_ADMIN_MAIL_ADDRESS)
-
-    print(f'메일 발송 결과 : {send_email_result}')
-
-
 def update_twitt_detail_info(status, user_name, twitt_user_info):
     """
     desc : Update 상세정보 - 트윗수, 좋아요수, 리트윗수
@@ -176,11 +81,11 @@ def create_twitt_info(create_at, status, user_type, twitt_days_info, twitt_hours
     # 계정유형별 자료 추가
     twitt_user_info[user_type] = twitt_detail_info
 
-    if exsits['date_exists'] == False:
+    if not exsits['hour_exists']:
         # 시간별 자료 추가
         twitt_hours_info[create_at.strftime('%H')] = twitt_user_info
 
-    if exsits['hour_exists'] == False:
+    if not exsits['date_exists']:
         # 일자별 자료 추가
         twitt_days_info[create_at_date] = twitt_hours_info
 
@@ -281,6 +186,101 @@ def main(keyword):
 
     # 자료 저장(스프레드시트) 및 메일 발송(gmail)
     save_data_on_spreadsheet(twitt_days_info)
+
+def save_data_on_spreadsheet(twitt_days_info):
+    """구글스프레드시트 저장"""
+    scope = [
+    'https://spreadsheets.google.com/feeds',
+    'https://www.googleapis.com/auth/drive',
+    ]
+
+    json_file_name = 'C:\GitHub\VNTG-N-ERP\emws\gspread.json'
+
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(json_file_name, scope)
+    gc = gspread.authorize(credentials)
+
+
+    spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1Z5yePPQLSJOpPxAOHWv4mTQJXxw_vUEjIFKBcqIzqA0/edit#gid=0'
+
+    # 스프레스시트 문서 가져오기 
+    doc = gc.open_by_url(spreadsheet_url)
+
+    # 스프레드시트 문서명
+    # worksheetName = datetime.strftime(now_time, '%Y%m')
+    worksheetName = f'{now_time.year}{str(now_time.month).zfill(2)}'
+
+    # 시트 선택하기
+    worksheet = doc.worksheet(worksheetName)
+
+    # 시트 자료 가져오기
+    worksheet_datas = worksheet.get_all_records()
+
+    # 1일이면 시트 생성
+    if now_time.day == 1:
+        # 시트 생성
+        worksheet = doc.add_worksheet(title=worksheetName, rows='1000', cols='22')
+        # 타이틀 추가
+        worksheet.append_row(constant.C_SPREADSHEET_TITLE)
+    
+    # 스프레드시트 작성
+    for post_date, hour_data in sorted(twitt_days_info.items()):
+        # print(post_date, hour_data)
+        for post_hour, campaign_data in sorted(hour_data.items()):
+            # print(post_date, post_hour, campaign_data)
+            for campaign, post_data in campaign_data.items():
+
+                # 추가 작성 필요 여부
+                appendYN = True
+
+                # 스프레드시트 내용 확인
+                for worksheet_data in worksheet_datas:
+                    # 스프레드시트의 일자/시간과 수집한 일자/시간과 일치하는지 확인
+                    if worksheet_data['게시일자'] == str(post_date) and worksheet_data['시간(24시)'] == int(post_hour):
+                        ##### 업데이트 필요
+                        # print('존재')
+                        appendYN = False
+
+                        # 스프레드시트 행 수 ( +2 : 인덱스 0부터 시작 / 타이틀 )
+                        rowCnt = worksheet_datas.index(worksheet_data) + 2
+
+                        # 스프레드시트 열
+                        colName = "H"
+
+                        # 게시일자 + n일이 수집일자와 같은지 확인
+                        for i in range(0, 7):
+                            if (datetime.strptime(worksheet_data['게시일자'], '%Y-%m-%d') + timedelta(days=i+1) == 
+                                datetime.strptime(datetime.strftime(now_time, '%Y-%m-%d'), '%Y-%m-%d')):
+                                # print('게시일자 + {}일이 수집일자와 동일'.format(i+1))
+                                colName = chr(ord(colName) + i)
+
+                                # 스프레드시트 작성
+                                worksheet.update_acell(f'{colName}{rowCnt}', list(post_data.values())[1] + list(post_data.values())[2])
+
+                                pass
+
+                if appendYN == True:
+                    ##### 신규 건 스프레드시트 작성
+                    print('신규')
+                    # 작성
+                    worksheet.append_row([
+                                        #   datetime.strftime(now_time, '%Y-%m-%d %H:%M:%S')
+                                        datetime.strftime(now_time, '%Y-%m-%d %H')
+                                        , str(post_date)
+                                        , str(post_hour)
+                                        , str(campaign)
+                                        , list(post_data.values())[0]
+                                        , list(post_data.values())[1]
+                                        , list(post_data.values())[2]
+                                        , "", "", "", "", "", "", ""
+                                        , "추가"
+                                        ])
+
+    print('스프레드시트 작성 종료')
+
+    # 메일 발송
+    send_email_result = send_email.send_email(constant.C_ADMIN_MAIL_ADDRESS)
+
+    print(f'메일 발송 결과 : {send_email_result}')
 
 if __name__ == '__main__':
 
--- note.py
+++ note.py
@@ -1,11 +1,11 @@
 from datetime import datetime, timedelta
 import constant
 
-str = "TTA AAAAAA"
+filter_string = "TTA AAAAAA"
 
 # 필터 문자열이 포함되는지 확인
 for filter_str in constant.C_FILTER_KEYWORD:
-    if filter_str in str:
+    if filter_str in filter_string:
         print('포함')
 
 # 일자별 트윗 정보 { '일자' : {'계정': {[게시글수, 좋아요수, 리트윗수]}}}
@@ -34,32 +34,41 @@ twittDaysInfo['2021-08-30'] = twittUserInfo
 
 twittDetailInfo1['writeCnt'] += 1
 
-print(twittDaysInfo)
+# print(twittDaysInfo)
 
-print(twittUserInfo)
+# print(twittUserInfo)
 
-print('hoursDic--------------------')
+# print('hoursDic--------------------')
 hoursDic = {}
 
 for i in range(0, 24, 1):
     hoursDic[i] = twittUserInfo
 
-print(hoursDic)
+# print(hoursDic)
 
 
 
 now = datetime.now().strftime('%Y')
-today = datetime.today()
+now_time = datetime.today()
 
 print(now)
-print(today)
+print(now_time)
+
+worksheetName = datetime.strftime(now_time, '%Y%m')
+print(worksheetName)
+worksheetName = str(now_time.year) + str(now_time.month).zfill(2)
+worksheetName = f'{now_time.year}{str(now_time.month).zfill(2)}'
+print(worksheetName)
+
+
+# print('now_time.hour : ' + str(now_time.hour) + str(now_time.minute))
 
 nowStr = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
-print(nowStr)
+# print(nowStr)
 
 path = "./history/{}.json".format(datetime.strftime(datetime.now(), '%y%m%d%H%M%S'))
 
-print(path)
+# print(path)
 
 if 'RT ' in 'RT @FROM_HW: #반민초 해시 걸고                   에피민트 광고하면서':
     print('true')
@@ -75,45 +84,45 @@ soup = outfile.read()
 test1 = '<p style="MARGIN-BOTTOM: 0px; MARGIN-TOP: 0px; LINE-HEIGHT: 150%"> PROCESS_NAME 프로세스가 수행되었습니다.</p>'
 test1 = 'PROCESS_NAME 프로세스가 수행되었습니다.'
 test1 = test1.replace("PROCESS_NAME", "test")
-print(test1)
+# print(test1)
 
 
 data_string = 'epimint asdf'
 filterList = bool([element for element in constant.C_FILTER_KEYWORD if(element in data_string)])
 
-print(filterList)
+# print(filterList)
 
 
 username = 'ads' if bool(filterList) is True else 'normal'
-print('test1 : ' + username)
+# print('test1 : ' + username)
 
 if bool(filterList):
     username = 'ads'
 else:
     username = 'normal'
 
-print('test2 : ' + username)
+# print('test2 : ' + username)
 
 
-print('{} {}'.format('에피민트', 'AND :ex'))
+# print('{} {}'.format('에피민트', 'AND :ex'))
 
-print(ord('H'))
-print(chr(ord('H') + 1))
-print(chr(ord('H') + 2))
+# print(ord('H'))
+# print(chr(ord('H') + 1))
+# print(chr(ord('H') + 2))
 
 stdDateStr = '2021-09-01 17'
-print(datetime.strptime(stdDateStr, '%Y-%m-%d %H'))
-print(datetime.strptime(stdDateStr, '%Y-%m-%d %H') + timedelta(hours=1))
-print(datetime.strptime(stdDateStr, '%Y-%m-%d %H') + timedelta(days=1))
-print(datetime.strftime(datetime.strptime(stdDateStr, '%Y-%m-%d %H') + timedelta(hours=1), '%Y-%m-%d %H'))
+# print(datetime.strptime(stdDateStr, '%Y-%m-%d %H'))
+# print(datetime.strptime(stdDateStr, '%Y-%m-%d %H') + timedelta(hours=1))
+# print(datetime.strptime(stdDateStr, '%Y-%m-%d %H') + timedelta(days=1))
+# print(datetime.strftime(datetime.strptime(stdDateStr, '%Y-%m-%d %H') + timedelta(hours=1), '%Y-%m-%d %H'))
 
 
-print(datetime.strftime(datetime.now(), '%Y-%m-%d %H'))
+# print(datetime.strftime(datetime.now(), '%Y-%m-%d %H'))
 # datetime.now()
 # timedelta(hours=9)
 
-print(datetime.strptime('2021-09-02', '%Y-%m-%d') + timedelta(days=1))
-print(datetime.strptime(datetime.strftime(datetime.now(), '%Y-%m-%d'), '%Y-%m-%d'))
+# print(datetime.strptime('2021-09-02', '%Y-%m-%d') + timedelta(days=1))
+# print(datetime.strptime(datetime.strftime(datetime.now(), '%Y-%m-%d'), '%Y-%m-%d'))
 
 # print(datetime.strftime('2021-09-02', '%Y-%m-%d'))
 
@@ -121,11 +130,11 @@ colName = "H"
 stdDateStr = '2021-08-27'
 nowtime = datetime.now()
 for i in range(0, 7):
-    print('i : {}'.format(i))
+    # print('i : {}'.format(i))
     if (datetime.strptime(stdDateStr, '%Y-%m-%d') + timedelta(days=i+1) == 
         datetime.strptime(datetime.strftime(nowtime, '%Y-%m-%d'), '%Y-%m-%d')):
-        print('게시일자 + {}일이 수집일자와 동일'.format(i+1))
-        print(chr(ord(colName) + i))
+        # print('게시일자 + {}일이 수집일자와 동일'.format(i+1))
+        # print(chr(ord(colName) + i))
         pass
 
 keyword = '에피민트'
@@ -134,11 +143,19 @@ extword = 'AND exclude:retweets'
 # extword = 'AND exclude:retweets AND filter:quote'
 # Full 검색키워드
 fullkeyword = f'{keyword} {extword}'
-print(fullkeyword)
+# print(fullkeyword)
 
 
 def test_kwargs(test1, **kwargs):
     print(test1)
     print(kwargs['keyword1234'])
 
-test_kwargs('test1', keyword1234='1234')
+# test_kwargs('test1', keyword1234='1234')
+
+
+test_dic = {'2021-09-03': {'14': {'normal': {'write_count': 1, 'like_count': 2, 'retwitt_count': 0}}}, '2021-09-02': {'15': {'normal': {'write_count': 1, 'like_count': 1, 'retwitt_count': 0}}}, '2021-09-01': {'22': {'normal': {'write_count': 1, 'like_count': 1, 'retwitt_count': 0}}}, '2021-08-31': {'23': {'normal': {'write_count': 1, 'like_count': 1, 'retwitt_count': 0}}}, '2021-08-30': {'11': {'normal': {'write_count': 1, 'like_count': 2, 'retwitt_count': 0}}}, '2021-08-29': {'23': {'normal': {'write_count': 1, 'like_count': 1, 'retwitt_count': 0}}}, '2021-08-28': {'23': {'normal': {'write_count': 1, 'like_count': 0, 'retwitt_count': 0}}}, '2021-08-27': {'22': {'normal': {'write_count': 1, 'like_count': 2, 'retwitt_count': 0}}}, '2021-08-26': {'21': {'normal': {'write_count': 1, 'like_count': 1, 'retwitt_count': 0}}}}
+
+test_nest = [testdic_key, testdic_value for testdic_key, testdic_value in test_dic.items()]
+
+for testdic_key, testdic_value in test_dic.items():
+    print(testdic_key, testdic_value)
