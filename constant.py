e93c4851925c2b6b5b4318285a8497ba92dae39e
hyunhee-lee <hyunhee.lee@vntgcorp.com>
2021-09-06 16:29:27 +0900

    CHG: 주석 처리

--- constant.py
+++ constant.py
@@ -1,11 +1,12 @@
+# ads - usertype 필터 문자열
 C_FILTER_KEYWORD = [
     'TTA',
     'epimint',
     'EPIMINT',
 ]
-
+# 프로젝트 절대경로
 C_ROOT_PATH = 'C:\GitHub\VNTG-N-ERP\emws'
-
+# 관리자 메일 주소
 C_ADMIN_MAIL_ADDRESS = 'hyunhee.lee@vntgcorp.com'
-
+# 수집 구글 스프레드시트 TITLE
 C_SPREADSHEET_TITLE = ['수집일시', '게시일자' '시간(24시)',	'채널',	'게시물 수', '좋아요 수', '리트윗 수', 'D+1 L&R', 'D+2 L&R', 'D+3 L&R', 'D+4 L&R', 'D+5 L&R', 'D+6 L&R', 'D+7 L&R', '비고']
